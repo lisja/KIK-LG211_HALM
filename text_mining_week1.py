702c8b0bcd174cd835c4936043c650b2c37bad30
mikzolot <mikhail.zolotilin@helsinki.fi>
2023-01-17 16:05:59 +0200

    Update text_mining_week1.py

--- text_mining_week1.py
+++ text_mining_week1.py
@@ -1,12 +1,13 @@
 from urllib import request
 from nltk import word_tokenize
 from bs4 import BeautifulSoup
+import ssl
 
 url = "https://www.bbc.com/future/article/20230116-how-donkeys-changed-the-course-of-human-history"
-html = request.urlopen(url).read().decode('utf8')
-# print(html[:600])
+#I added ssl to fix the error I was getting (urllib.error.URLError: <urlopen error [SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed: unable to get local issuer certificate (_ssl.c:997)>)
+context = ssl._create_unverified_context()
+html = request.urlopen(url, context=context).read().decode('utf8')
 
 raw = BeautifulSoup(html, 'html.parser').get_text()
 tokens = word_tokenize(raw)
 print(tokens)
-
