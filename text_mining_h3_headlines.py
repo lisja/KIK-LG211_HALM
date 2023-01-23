492737f58b987bacc986e1791044a657dc371668
Liisa Jalkanen <liisajal@puhti-login12.bullx>
2023-01-23 09:24:34 +0200

    Created a program to find all the h3 sized headlines from a changing source, i.e. BBC front page

---
+++ text_mining_h3_headlines.py
@@ -0,0 +1,19 @@
+# program to find all the h3 sized headlines from the BBC front page
+
+from bs4 import BeautifulSoup
+import requests
+
+url = "https://www.bbc.com"
+req = requests.get(url)
+soup = BeautifulSoup(req.text, "html.parser")
+
+
+try:
+    content_div = soup.find_all("h3", class_="media__title")
+
+    for i in content_div:
+        contents = i.get_text("\n")
+        print(contents)
+
+except:
+    print("error happened")
