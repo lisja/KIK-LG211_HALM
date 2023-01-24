fb165780df6b7996772830e50efb979e79da9c47
mikzolot <mikhail.zolotilin@helsinki.fi>
2023-01-24 21:12:32 +0200

    Update HALM_search_engine.py

--- WEEK_2/HALM_search_engine.py
+++ WEEK_2/HALM_search_engine.py
@@ -58,19 +58,28 @@ def readandcut(file_path):
     with open(file_path, "r") as file:
         text = file.read()
 
-   
     text = str(text)
 
-    
-    cuttext = re.split(r"</article>", text)
-    return cuttext
-
+    cuttext = text.split("</article>")
+    cuttext = cuttext[:-1]
+    return text, cuttext
 
 file_path = "WEEK_2/enwiki-20181001-corpus.100-articles.txt"
-cuttext = readandcut(file_path)
+text, cuttext = readandcut(file_path)
 
-print(cuttext[0])
+def word_set(article):
+    article = article.lower()
+    article = re.sub(r"[^a-z\s]", "", article)
+    words = article.split()
+    return set(words)
 
+minisets = [word_set(i) for i in cuttext]
+mainset = word_set(text)
+vectors = []
+for article_set in minisets:
+    vector = [1 if word in article_set else 0 for word in mainset]
+    vectors.append(vector)
+print(vectors[1])
             
 def main():
     interface()
