e8218847a04bf50e34bf7d1cf7b27c9f5ed23f9b
mikzolot <mikhail.zolotilin@helsinki.fi>
2023-01-24 13:51:41 +0200

    Update HALM_search_engine.py

--- WEEK_2/HALM_search_engine.py
+++ WEEK_2/HALM_search_engine.py
@@ -25,6 +25,27 @@ def interface():
             break
         else:
             search(input_query)
+
+import re
+
+def readandcut(file_path):
+    with open(file_path, "r") as file:
+        text = file.read()
+
+   
+    text = str(text)
+
+    
+    cuttext = re.split(r"<", text)
+    cuttext.pop(0)
+    return cuttext
+
+
+file_path = "/GitHub/KIK-LG211_HALM/WEEK_2/enwiki-20181001-corpus.100-articles.txt"
+cuttext = readandcut(file_path)
+
+print(cuttext[0])
+
             
 
 def main():
