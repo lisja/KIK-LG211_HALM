b56623d5bf47b8facf254cf3370f0170ffe53eb5
mikzolot <mikhail.zolotilin@helsinki.fi>
2023-01-24 22:04:43 +0200

    Update HALM_search_engine.py

--- WEEK_2/HALM_search_engine.py
+++ WEEK_2/HALM_search_engine.py
@@ -60,14 +60,15 @@ def search(input_query):
     t2i = cv.vocabulary_
 
     print("Query: '" + input_query + "'")
-
+    #task 2 done
     try:
         print("Rewritten:", rewrite_query(input_query))
         print("Matching:", eval(rewrite_query(input_query)))
         hits_matrix = eval(rewrite_query(input_query))
         hits_list = list(hits_matrix.nonzero()[1])
         for i, doc_idx in enumerate(hits_list):
-            print("Matching doc #{:d}: {:s}".format(i, documents[doc_idx])) # Eval runs the string as a Python command
+            first_line = documents[doc_idx].splitlines()[1]
+            print("Matching doc #{:d} - {:s}".format(i, first_line))
         print()
     except KeyError:
         print("Query not found in the documents.")
