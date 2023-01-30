78805852f41918e2cd485bd108ec2ed5d0d32bfe
Alex Lyysaari <Alex@MacBook-Pro-3.home>
2023-01-30 12:09:53 +0200

    cleaned output, anarchism works now, cleaned the code slightly

--- WEEK_2/HALM_search_engine.py
+++ WEEK_2/HALM_search_engine.py
@@ -1,6 +1,20 @@
 from sklearn.feature_extraction.text import CountVectorizer
 import re
 
+def linesplitter_and_cleaner(document):
+
+    #making a list of lines of the document
+
+    doc_lines = document.splitlines()
+
+    #removing lines that are empty
+
+    for line in doc_lines:
+        if len(line) < 1:
+            doc_lines.remove(line)
+
+    return doc_lines
+
 def readandcut(file_path):
     with open(file_path, "r") as file:
         text = file.read()
@@ -11,8 +25,102 @@ def readandcut(file_path):
     cuttext = cuttext[:-1]
     return text, cuttext
 
+def rewrite_token(t):
+    return d.get(t, 'td_matrix[t2i["{:s}"]]'.format(t)) # rewrites tokens
+
+def rewrite_query(query): # rewrite every token in the query
+    return " ".join(rewrite_token(t) for t in query.split())
+
+def search(input_query): # search the query
+
+    #Reminder about the commands; print removed to make the output clearer
+    """print("Commands 'AND', 'NOT', and 'OR' always upper case!")"""
+    
+    cv = CountVectorizer(lowercase=True, binary=True, token_pattern=r'[A-Za-z0-9]+\b')
+    sparse_matrix = cv.fit_transform(documents)
+    dense_matrix = sparse_matrix.todense()
+    td_matrix = dense_matrix.T
+    t2i = cv.vocabulary_
+
+    try:
+
+        #Remove """ if you'd like to see how the query is rewritten and the matrix
+        """print("Rewritten:", rewrite_query(input_query))
+        print("Matching:", eval(rewrite_query(input_query)))"""
+        
+        hits_matrix = eval(rewrite_query(input_query))
+        hits_list = list(hits_matrix.nonzero()[1])
+
+        print("\nThere are/is ", len(hits_list), " hit(s).\n")
+        print("-"*30, end="\n\n\n")
+
+        
+        for i, doc_idx in enumerate(hits_list):
+
+#This is Alex's revised code 30.1.2023
+            
+            doc_lines = linesplitter_and_cleaner(documents[doc_idx])
+
+            article_name = doc_lines[0]
+            first_line = doc_lines[1]
+
+            #Delete the article name tag from the article_name
+            article_name = re.sub(r'<article name="(.*?)">', r'\1', article_name)
+            
+            print("Article: {:s}\n\nContent: {:s}\n\n".format(article_name, first_line))
+            print("-"*30)
+            
+#This is Mikhail's original code
+            
+            """
+            documents[doc_idx].splitlines()
+            first_line = documents[doc_idx].splitlines()[1]
+            print("Matching doc #{:d} - {:s}".format(i, first_line))
+            """
+            
+        print()
+    except KeyError:
+        print("Query not found in the documents.")
+    except SyntaxError:
+        print("'AND', 'NOT', and 'OR' are commands. Use lowercase, e.g. 'and', 'not', or 'or'")
+    print()
+
+def interface():
+
+    welcome_message = "Welcome to HALM search engine!"
+
+    print()
+    print("*"*len(welcome_message))
+    print(welcome_message)
+    print("*"*len(welcome_message))
+    print()
+    
+    while True:
+    
+        print("'q' = Quit; use only lower-case unless using the AND, OR and NOT commands.")
+        input_query = input("Input your query: ")
+
+        if input_query == "q":
+
+            quit_message = "Quitting program. Thank you for using the HALM Search Engine!"
+            
+            print()
+            print("*"*len(quit_message))
+            print(quit_message)
+            print("*"*len(quit_message))
+            print()
+            
+            break
+        else:
+            search(input_query)
+
+def main():
+    
+    interface()
+
 file_path = "enwiki-20181001-corpus.100-articles.txt"
 text, documents = readandcut(file_path)
+
 #these basically do the same thing that sklearn...dont ask me why I added it, but maybe will be
 #useful later, so lets keep it...
 """def word_set(article):
@@ -27,9 +135,6 @@ vectors = []
 for article_set in minisets:
     vector = [1 if word in article_set else 0 for word in mainset]
     vectors.append(vector)"""
-cv = CountVectorizer(lowercase=True, binary=True)
-sparse_matrix = cv.fit_transform(documents)
-print(sparse_matrix) 
 
 d = {"AND": "&",
      "OR": "|",
@@ -37,63 +142,9 @@ d = {"AND": "&",
      "(": "(",
      ")": ")"}
 
-"""documents = ["This is a silly example",
-             "A better example",
-             "Nothing to see here",
-             "This is a great and long example"]"""
-
-def rewrite_token(t):
-    return d.get(t, 'td_matrix[t2i["{:s}"]]'.format(t)) # rewrites tokens
-
-def rewrite_query(query): # rewrite every token in the query
-    return " ".join(rewrite_token(t) for t in query.split())
-
-def search(input_query): # search the query
-    print("Searching...\n\n")
-    print("Commands 'AND', 'NOT', and 'OR' always upper case!")
-    
-    cv = CountVectorizer(lowercase=True, binary=True, token_pattern=r'[A-Za-z0-9]+\b')
-    sparse_matrix = cv.fit_transform(documents)
-    dense_matrix = sparse_matrix.todense()
-    td_matrix = dense_matrix.T
-    t2i = cv.vocabulary_
-
-    print("Query: '" + input_query + "'")
-    #task 2 done
-    try:
-        print("Rewritten:", rewrite_query(input_query))
-        print("Matching:", eval(rewrite_query(input_query)))
-        hits_matrix = eval(rewrite_query(input_query))
-        hits_list = list(hits_matrix.nonzero()[1])
-        for i, doc_idx in enumerate(hits_list):
-            first_line = documents[doc_idx].splitlines()[1]
-            print("Matching doc #{:d} - {:s}".format(i, first_line))
-        print()
-    except KeyError:
-        print("Query not found in the documents.")
-    except SyntaxError:
-        print("'AND', 'NOT', and 'OR' are commands. Use lowercase, e.g. 'and', 'not', or 'or'")
-    print()
-
-def interface():
-    print("Welcome to HALM search engine!\n")
-
-    while True:
-    
-        print("'q'= Quit")
-        input_query = input("Input your query: ")
-
-        if input_query == "q":
-            break
-        else:
-            search(input_query)
-
-
-
-
-
-def main():
-    interface()
+#THESE TWO LINES ARE UNNECESSARY:
+#cv = CountVectorizer(lowercase=True, binary=True)
+#sparse_matrix = cv.fit_transform(documents)
 
 main()
 
