d9750c137deaff0283117cdce9d491650c6be817
Haralds Matulis <haralds.matulis@gmail.com>
2023-02-21 13:22:12 +0200

    continued Visualizations folder – as it is - work in progress

--- WEEK_5/visualizations/three_heroes_timeline.py
+++ WEEK_5/visualizations/three_heroes_timeline.py
@@ -5,6 +5,7 @@ import collections
 from matplotlib import pyplot as plt
 import re
 import wordcloud
+from sklearn.feature_extraction.text import CountVectorizer
 
 
 # naruto_txt = "https://raw.githubusercontent.com/lisja/KIK-LG211_HALM/main/WEEK_5/naruto3.txt"
@@ -38,7 +39,7 @@ heroes = {
   'Jiraiya': 54, 'Sannin': 28, 'Kikunojou': 9, 'Kikusuke': 4, 'Haruna': 9, 
   'Janin': 3, 'Ebisu': 5, 'Boss': 29,  'Grandson': 3, 
   'Haruno': 7, 'Inuzuka': 7, 'Aburame': 9, 
-  'Nara': 6, 'Akimichi': 5, 'Shinobi"': 2, 
+  'Nara': 6, 'Akimichi': 5, 'Shinobi': 2, 
   'Jiga': 2, 'Ayame': 14, 'Fuma': 3, 'Sasame': 37, 'Arashi': 21, 
   'Wolf': 2, 'Temari': 22, 'Kankuro': 16, 
   'Shukaku': 13,'Kunihisa': 1, 'Tokichi': 3, 
@@ -53,52 +54,102 @@ heroes = {
   'Mizura': 16, 'Sumaru': 42, 'Hoshikage': 15,'Dojo': 11, 
   'Yurinojou': 3, 'Morino': 6, 'Mitarashi': 1, 'Gatô': 2,  
   'Kagetsu': 3, 'Futa': 3, 'Akahoshi': 39, 'Hokuto': 6, 
-  'Kagero': 2, 'U-zu-ma-ki': 1, 'Fukusuke': 5,  'Rokusuke': 11, 'Raiga': 28, 'Karashi': 26, 'Ranmaru': 26,
+  'Kagero': 2, 'U-zu-ma-ki': 1, 'Fukusuke': 5, 'Rokusuke': 11, 'Raiga': 28, 'Karashi': 26, 'Ranmaru': 26,
   'Wagarashi': 4, 'Onbaa': 10, 'Yamanaka': 3, 'Onbu': 1, 
   'Sansho': 7, 'Kanpachi': 1, 'Degarashi': 2, 'Natsuhi': 7, 
   'Yagura': 10, 'Gekko': 1, 'Kubisaki': 4, 'Gosa': 1, 'Kurosuki': 1, 
   'Agira': 1, 'Wasabi': 1
 }
+print("len(heroes): ", len(heroes))
 
-def get_frequency_list(data_url):
+def readandcut(file_path):
+    with open(file_path, "r") as file:
+        text = file.read()
 
-  with open(data_url, "r") as file:
-    text = file.read()
+    text = str(text)
 
-    words = []
-    lines = text.split("\n")
-    for line in lines:
-      # TODO – add line to include only lines starting with non-digit
-      # if not line[0].isdigit():
-      # array = line.split(" ")
-      array = re.split(" |'|’", line)
-      # print(array)
-      # print(type(array))
-      for item in array:
-        if item.lower() != item:
-          words.extend([item])
-      # array = [item for item in array if item[0] == item.upper()[0]]
-      # array = [item for item in array if item[0] == item.upper()[0]]
-      # words.extend(array)
-      # words.extend(line.split(" "))
+    cuttext = text.split("//%")
+    cuttext = cuttext[:-1]
+    return text, cuttext
+
+text, documents = readandcut(naruto_txt)
+# print("text: ", documents)
+
+d = {"AND": "&",
+     "OR": "|",
+     "NOT": "1 -",
+     "(": "(",
+     ")": ")"}
+
+def rewrite_token(t):
+    return d.get(t, 'td_matrix[t2i["{:s}"]]'.format(t)) # rewrites tokens
+
+def rewrite_query(query): # rewrite every token in the query
+    return " ".join(rewrite_token(t) for t in query.split())
+
+cv = CountVectorizer(lowercase=False, binary=True, token_pattern=r'[A-Za-z0-9_À-ÿ\-]+\b')
+sparse_matrix = cv.fit_transform(documents)
+# print("sparse_matrix: ", sparse_matrix)
+dense_matrix = sparse_matrix.todense()
+# print("dense_matrix: ", dense_matrix)
+td_matrix = dense_matrix.T
+t2i = cv.vocabulary_
+# print("t2i: ", t2i)
+
+large_dict = {}
+for key, value in heroes.items():
+  # print("key, value: ", key, value)
+# hits_matrix = eval(rewrite_query("Naruto"))
+  hits_matrix = eval(rewrite_query(key))
+  hits_list = list(hits_matrix.nonzero()[1])
+  large_dict[key] = hits_list
+# print(large_dict)
+
+# sort Heroes in the order of appearance in series
+sorted_list = [[key, value] for key, value in large_dict.items()]
+sorted_list = sorted(sorted_list, key=lambda x: x[1])
+# print(sorted_list)
+
+
+# create x,y data_points for every 
+x_values = []
+y_values = []
+for my_list in sorted_list:
+  # print(my_list)
+  for el in my_list[1]:
+    x_values.append(el)
+    y_values.append(my_list[0])
     
-    frequency_list = dict(collections.Counter(words))
-    filtered_frequency_list = dict()
-    for k,v in frequency_list.items():
-      stripped = re.sub("[,\.:;-?!…]", "", k)
-      if frequency_list.get(stripped, 0) > v and stripped in heroes:
-        filtered_frequency_list[stripped] = filtered_frequency_list.get(stripped, 0) + v
+# print(len(x_values))
+# print(len(y_values))
 
-  # TODO – add line to check IF word not in STOPWORDS list, and then only include
-  # TODO – stopwords list need to be created
+y = y_values
+# # print(y)
+x = x_values
+# print(x)
 
-    return {k:v for k,v in filtered_frequency_list.items() if len(k) > 1}
+f = plt.figure(figsize=(50,30))
+ax1 = f.add_subplot(111)
 
 
-DATA = naruto_txt
-freqs = get_frequency_list(DATA)
-print("freqs: ", freqs)
+# # # # set formatter and locator of ticks for x axis
+# # # ax1.xaxis.set_major_formatter(formatter)
+# # # tick_spacing = 1825
+# # # ax1.xaxis.set_major_locator(ticker.MultipleLocator(tick_spacing))
+# # # ax1.xaxis.grid(True, which='major')
+# # # ax1.yaxis.grid(True, which='major')
 
-wcloud = wordcloud.WordCloud().generate_from_frequencies(freqs)
-plt.imshow(wcloud)
+# # # TODO – set red alpha for Naruto / and maybe others
+ax1.scatter(x,y, alpha=0.3)
+
+# # set limits of y axis
+# # TODO - see if next line necessary
+ax1.set_ylim(len(list(set(y)))+1,-1)
+    
+ax1.set_ylabel('Heroes in order of appearance', fontsize=18)
+ax1.set_xlabel('Series Nr', fontsize=18)
+# ax1.set_title('Distribution of diaries\' entries over years', fontsize=24)
+
+# # plt.xticks(rotation=90)
 plt.show()
+
