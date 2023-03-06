3e66813b00ae9a7e822374026edc80488b1bb5fc
Haralds Matulis <haralds.matulis@gmail.com>
2023-03-06 09:55:53 +0200

    modify wordclouds to remove axis and ticks

--- FINAL_PROJECT/visualizations/two_wordcloud_heroes.py
+++ FINAL_PROJECT/visualizations/two_wordcloud_heroes.py
@@ -7,7 +7,10 @@ from matplotlib import pyplot as plt
 import wordcloud
 from data_processing_file import heroes_dict
 
-
-wcloud = wordcloud.WordCloud().generate_from_frequencies(heroes_dict)
-plt.imshow(wcloud)
+wcloud = wordcloud.WordCloud(width=500, height=400).generate_from_frequencies(heroes_dict)
+# plt.imshow(wcloud)
+plt.imshow(wcloud, aspect="equal")
+# next line - to remove axis, ticks and numbers around wordcloud
+plt.axis("off")
+plt.savefig('two_wordcloud_heroes.png', figsize=(15,5), dpi=100)
 plt.show()
